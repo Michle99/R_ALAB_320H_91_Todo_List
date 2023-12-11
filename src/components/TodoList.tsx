73dd8a426b38c206b5235e153d043c628e9a70e5
Michle99 <sammiyonyx@gmail.com>
2023-12-11 16:20:44 -0500

    Updated Dependencies

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -11,11 +11,11 @@ import {
 // import { todoReducer, initialState, TodoAction } from '../reducers/todoReducers';
 
 const TodoList: React.FC = () => {
-  const [state, dispatch] = useReducer(todoReducer, initialState);
+//   const [state, dispatch] = useReducer(todoReducer, initialState);
 
-  const addTodo = (text: string) => {
-    dispatch({ type: TodoAction.ADD_TODO, payload: text });
-  };
+//   const addTodo = (text: string) => {
+//     dispatch({ type: TodoAction.ADD_TODO, payload: text });
+//   };
 
   return (
     <ChakraProvider>
@@ -41,10 +41,10 @@ const TodoList: React.FC = () => {
           align="stretch"
           spacing="4"
         >
-          <TodoInput addTodo={addTodo} />
+          {/* <TodoInput addTodo={addTodo} />
           {state.todos.map((todo) => (
             <TodoItem key={todo.id} todo={todo} dispatch={dispatch} />
-          ))}
+          ))} */}
         </VStack>
       </Box>
     </ChakraProvider>
