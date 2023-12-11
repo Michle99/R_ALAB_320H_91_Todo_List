864b1872918aa6879713236681aba3b88efaadaf
Michle99 <sammiyonyx@gmail.com>
2023-12-11 16:23:40 -0500

    updated TodoInput

---
+++ src/components/TodoInput.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { Input, Button, Flex } from '@chakra-ui/react';
+import { AddIcon } from '@chakra-ui/icons';
+
+interface TodoInputProps {
+  addTodo: (text: string) => void;
+}
+
+const TodoInput: React.FC<TodoInputProps> = ({ addTodo }) => {
+  const [text, setText] = useState('');
+
+  const handleAddTodo = () => {
+    if (text.trim() !== '') {
+      addTodo(text);
+      setText('');
+    }
+  };
+
+  return (
+    <Flex 
+      mb="4"
+    >
+      <Input
+        type="text"
+        placeholder="Add a new todo"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        mr="2"
+      />
+      <Button 
+        onClick={handleAddTodo} 
+        colorScheme="teal"
+        ml="4"
+        leftIcon={<AddIcon/>}
+      >
+        Add Todo
+      </Button>
+    </Flex>
+  );
+};
+
+export default TodoInput;
